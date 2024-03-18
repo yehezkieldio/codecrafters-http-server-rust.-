c3a852c84ae9d85059bdaab1eac3351dc41be78b
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-03-18 22:18:44 +0800

    feat: pass 6th stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    error::Error,
     io::{Read, Write},
     net::TcpListener,
 };
@@ -37,77 +38,73 @@ impl HTTPStatus {
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut tcp_stream) => {
-                println!("Connection established!");
-                let mut buffer = [0; 1024];
+    loop {
+        let (mut tcp_stream, _) = listener.accept().unwrap();
+        tokio::spawn(async move {
+            println!("Connection established!");
+            let mut buffer = [0; 1024];
 
-                match tcp_stream.read(&mut buffer) {
-                    Ok(0) => {
-                        println!("Client closed the connection.");
-                        continue;
-                    }
-                    Ok(_) => {
-                        let request = String::from_utf8_lossy(&buffer[..]);
-                        println!("Received request: {}", request);
+            match tcp_stream.read(&mut buffer) {
+                Ok(0) => {
+                    println!("Client closed the connection.");
+                    return;
+                }
+                Ok(_) => {
+                    let request = String::from_utf8_lossy(&buffer[..]);
+                    println!("Received request: {}", request);
 
-                        let response = match request.split_whitespace().nth(1) {
-                            Some("/") => HTTPStatus::Ok(
-                                "Hello, world!".to_string(),
-                                "text/plain".to_string(),
-                            ),
-                            Some("/user-agent") => {
-                                let user_agent = request
-                                    .split("\r\n")
-                                    .find(|line| line.starts_with("User-Agent"))
-                                    .map(|line| line.split(": ").nth(1).unwrap_or("Unknown"))
-                                    .unwrap_or("Unknown")
-                                    .to_string();
-
-                                println!("User-Agent: {}", user_agent);
-                                HTTPStatus::Ok(user_agent, "text/plain".to_string())
-                            }
-                            Some(path) if path.starts_with("/echo/") => {
-                                let echo_string = &path[6..];
-
-                                println!("Echoing: {}", echo_string);
-                                HTTPStatus::Ok(echo_string.to_string(), "text/plain".to_string())
-                            }
-                            _ => HTTPStatus::NotFound,
-                        };
-
-                        match tcp_stream.write(response.to_string().as_bytes()) {
-                            Ok(_) => {
-                                println!("Response sent!");
-                            }
-                            Err(e) => {
-                                println!("Failed to send response: {}", e);
-                            }
+                    let response = match request.split_whitespace().nth(1) {
+                        Some("/") => {
+                            HTTPStatus::Ok("Hello, world!".to_string(), "text/plain".to_string())
                         }
+                        Some("/user-agent") => {
+                            let user_agent = request
+                                .split("\r\n")
+                                .find(|line| line.starts_with("User-Agent"))
+                                .map(|line| line.split(": ").nth(1).unwrap_or("Unknown"))
+                                .unwrap_or("Unknown")
+                                .to_string();
 
-                        match tcp_stream.flush() {
-                            Ok(_) => {
-                                println!("Flushed the stream!");
-                            }
-                            Err(e) => {
-                                println!("Failed to flush the stream: {}", e);
-                            }
+                            println!("User-Agent: {}", user_agent);
+                            HTTPStatus::Ok(user_agent, "text/plain".to_string())
+                        }
+                        Some(path) if path.starts_with("/echo/") => {
+                            let echo_string = &path[6..];
+
+                            println!("Echoing: {}", echo_string);
+                            HTTPStatus::Ok(echo_string.to_string(), "text/plain".to_string())
+                        }
+                        _ => HTTPStatus::NotFound,
+                    };
+
+                    match tcp_stream.write(response.to_string().as_bytes()) {
+                        Ok(_) => {
+                            println!("Response sent!");
+                        }
+                        Err(e) => {
+                            println!("Failed to send response: {}", e);
                         }
                     }
-                    Err(e) => {
-                        println!("Failed to receive data: {}", e);
+
+                    match tcp_stream.flush() {
+                        Ok(_) => {
+                            println!("Flushed the stream!");
+                        }
+                        Err(e) => {
+                            println!("Failed to flush the stream: {}", e);
+                        }
                     }
                 }
+                Err(e) => {
+                    println!("Failed to receive data: {}", e);
+                }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
 
