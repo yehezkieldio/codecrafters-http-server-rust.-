9301c77df30b261ec42f1bf0b91fbeb2ea038351
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-03-18 20:59:29 +0800

    feat: pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,16 @@
-// Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     //
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut tcp_stream) => {
+                tcp_stream
+                    .write("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                    .unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
