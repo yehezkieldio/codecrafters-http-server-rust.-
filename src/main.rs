9f78f6e87bbe69a558067fd446f95e9eb0b6487b
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-03-18 23:29:34 +0800

    feat: pass 8th stage

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ use std::{
     net::TcpListener,
 };
 
+use tokio::{fs::File, io::AsyncWriteExt};
+
 pub enum HTTPStatus {
     Ok(String, String),
     NotFound,
+    Created,
 }
 
 impl HTTPStatus {
@@ -14,6 +17,7 @@ impl HTTPStatus {
         match self {
             HTTPStatus::Ok(_, content_type) => content_type.clone(),
             HTTPStatus::NotFound => "text/plain".to_string(),
+            HTTPStatus::Created => "text/plain".to_string(),
         }
     }
     pub fn to_string(&self) -> String {
@@ -24,6 +28,14 @@ impl HTTPStatus {
                     self.content_type(),
                     body.len(),
                     body
+                )
+            }
+            HTTPStatus::Created => {
+                format!(
+                    "HTTP/1.1 201 CREATED\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+                    self.content_type(),
+                    0,
+                    ""
                 )
             }
             HTTPStatus::NotFound => {
@@ -68,7 +80,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let (mut tcp_stream, _) = listener.accept().unwrap();
         tokio::spawn(async move {
             println!("Connection established!");
-            let mut buffer = [0; 1024];
+            let mut buffer: [u8; 4096] = [0; 4096];
             let directory_flag = std::env::args().nth(2);
 
             match tcp_stream.read(&mut buffer) {
@@ -106,13 +118,51 @@ async fn main() -> Result<(), Box<dyn Error>> {
                             match directory_flag {
                                 Some(directory) => {
                                     let file_path = format!("{}/{}", directory, file_name);
-                                    println!("File path: {}", file_path);
-                                    match tokio::fs::read_to_string(file_path).await {
-                                        Ok(file_content) => HTTPStatus::Ok(
-                                            file_content,
-                                            "application/octet-stream".to_string(),
-                                        ),
-                                        Err(_) => HTTPStatus::NotFound,
+
+                                    if request.split_whitespace().nth(0) == Some("POST") {
+                                        println!("POST request received!");
+
+                                        let file_content =
+                                            request.split("\r\n\r\n").nth(1).unwrap_or("");
+                                        println!("File content: {}", file_content);
+
+                                        let file_content = file_content
+                                            .split("\r\n")
+                                            .map(|line| line.trim())
+                                            .collect::<Vec<&str>>()
+                                            .join("\n");
+                                        println!("File content: {}", file_content);
+
+                                        match File::create(file_path).await {
+                                            Ok(mut file) => {
+                                                match file.write_all(file_content.as_bytes()).await
+                                                {
+                                                    Ok(_) => {
+                                                        println!("File content written!");
+                                                        HTTPStatus::Created
+                                                    }
+                                                    Err(e) => {
+                                                        println!(
+                                                            "Failed to write file content: {}",
+                                                            e
+                                                        );
+                                                        HTTPStatus::NotFound
+                                                    }
+                                                }
+                                            }
+                                            Err(e) => {
+                                                println!("Failed to create file: {}", e);
+                                                HTTPStatus::NotFound
+                                            }
+                                        }
+                                    } else {
+                                        match tokio::fs::read_to_string(file_path).await {
+                                            Ok(file_content) => HTTPStatus::Ok(
+                                                file_content,
+                                                "application/octet-stream".to_string(),
+                                            ),
+                                            Err(_) => HTTPStatus::NotFound,
+                                        }
                                     }
                                 }
                                 None => HTTPStatus::NotFound,
@@ -144,22 +194,5 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         });
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_content_type_ok() {
-        let status = HTTPStatus::Ok("Hello, world!".to_string(), "text/plain".to_string());
-        assert_eq!(status.content_type(), "text/plain");
-    }
-
-    #[test]
-    fn test_content_type_not_found() {
-        let status = HTTPStatus::NotFound;
-        assert_eq!(status.content_type(), "text/plain");
     }
 }
